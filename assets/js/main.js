f0b996535061ed2d5e96e1b579b882275e62362c
Matheus Carvalho <matheus.carvalho.corp@gmail.com>
2026-01-25 02:50:37 +0900

    fix: Ajustes no ponteiro do mouse para itens clicaveis e ajustes de responsividade

--- assets/js/main.js
+++ assets/js/main.js
@@ -7,6 +7,7 @@ let offset = 0;
 
 function convertPokemonToLi(pokemon) {
     return `
+        <a href="pokemon-details.html?id=${pokemon.number}"
         <li class="pokemon ${pokemon.type}">
             <span class="number">#${pokemon.number}</span>
             <span class="name">${pokemon.name}</span>
@@ -20,6 +21,7 @@ function convertPokemonToLi(pokemon) {
                      alt="${pokemon.name}">
             </div>
         </li>
+        </a>
     `
 }
 
